729beb0843550f66b213d0c349bb92cf53cc7b9f
student <mukherjeei@outlook.co.nz>
2019-02-04 12:23:49 +1300

    add new user working

--- routes/users.js
+++ routes/users.js
@@ -4,10 +4,11 @@ const db = require('../db')
 
 const router = express.Router()
 
+
 router.get('/', (req, res) => {
   db.getUsers()
     .then(users => {
-      res.send({users: users})
+      res.send({ users: users })
     })
     .catch(err => {
       res.status(500).send('DATABASE ERROR: ' + err.message)
@@ -18,10 +19,20 @@ router.get('/:id', (req, res) => {
   const id = Number(req.params.id)
   db.getUser(id)
     .then(user => {
-      res.json({user: user})
+      res.json({ user: user })
     })
     .catch(err => {
       res.status(500).send('DATABASE ERROR: ' + err.message)
+    })
+})
+
+router.post('/addnew', (req, res) => {
+  const newUser = req.body
+
+  console.log(newUser)
+  db.addUser(newUser)
+    .then(() => {
+      res.redirect('/')
     })
 })
 
